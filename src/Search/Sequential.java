f5b9b2cfad646c473781c0a76b1dc24991b82878
Andragons <49853842+Andragons@users.noreply.github.com>
2021-04-06 16:04:10 +0700

    Update Sequential.java

--- src/Search/Sequential.java
+++ src/Search/Sequential.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class Sequential {
     
     public static void main(String[] args) {
+         System.out.println("========== Sequential Search ==========");
         String A[] = {"Galileo", "Archimedes", "Alkhawarizmi", "Aljabar", "Ramanda", "Tesla"};
         String cari;
         Scanner scan = new Scanner(System.in);
